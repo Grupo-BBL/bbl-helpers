38303fc8f099ca06710525c19c90c7df6333fb69
Gustavo Tavares <gustavo@froml.com>
2025-03-19 18:31:39 -0400

    Make an if optional :)

--- src/Functions/OSFunctions.php
+++ src/Functions/OSFunctions.php
@@ -2,12 +2,12 @@
 
 if (!function_exists("isWindows"))
 {
-	function isWindows() 
-	{
-		if (strcasecmp(substr(PHP_OS, 0, 3), 'WIN') == 0){
-			return true;
-		}
-	
-		return false;
-	}
+function isWindows() 
+{
+if (strcasecmp(substr(PHP_OS, 0, 3), 'WIN') == 0){
+return true;
+}
+
+return false;
+}
 }
